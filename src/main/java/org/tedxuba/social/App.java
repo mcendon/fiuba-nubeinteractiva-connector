a79bb637ce2d5b6596520b695ba961b823291b29
Fernando Sproviero <fernandospr@gmail.com>
2013-10-03 23:18:43 -0300

    Simple Facebook example

--- src/main/java/org/tedxuba/social/App.java
+++ src/main/java/org/tedxuba/social/App.java
@@ -1,13 +1,64 @@
 package org.tedxuba.social;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Key;
+
+public class App {
+	static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+	static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+	public static class Feed {
+		@Key("like_count")
+		public int likeCount;
+	}
+
+	public static class FacebookUrl extends GenericUrl {
+
+		public FacebookUrl(String encodedUrl) {
+			super(encodedUrl);
+		}
+
+		@Key
+		public String fields;
+	}
+
+	private static void run() throws Exception {
+		HttpRequestFactory requestFactory = HTTP_TRANSPORT
+				.createRequestFactory(new HttpRequestInitializer() {
+					public void initialize(HttpRequest request)
+							throws IOException {
+						request.setParser(new JsonObjectParser(JSON_FACTORY));
+					}
+				});
+		FacebookUrl url = new FacebookUrl(
+				"https://api.facebook.com/method/fql.query?format=json&query=select%20like_count%20from%20link_stat%20WHERE%20url%20%3D%27www.facebook.com/TEDxUBA%27");
+		// url.fields = "id,tags,title,url";
+		HttpRequest request = requestFactory.buildGetRequest(url);
+		System.out.println(request.execute().parseAsString());
+	}
+
+	public static void main(String[] args) {
+		try {
+			try {
+				run();
+				return;
+			} catch (HttpResponseException e) {
+				System.err.println(e.getMessage());
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		System.exit(1);
+	}
 }
