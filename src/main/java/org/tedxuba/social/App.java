b1bca6f208e125398f4761e9cc7b436c0ef75299
Fernando Sproviero <fernandospr@gmail.com>
2013-10-04 09:58:31 -0300

    Simple Facebook example, querying loop

--- src/main/java/org/tedxuba/social/App.java
+++ src/main/java/org/tedxuba/social/App.java
@@ -6,7 +6,6 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -14,18 +13,22 @@ import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Key;
 
+/**
+ * Hello world!
+ * 
+ */
 public class App {
 	static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 	static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-	public static class Feed {
+	public static class VideoFeed {
 		@Key("like_count")
 		public int likeCount;
 	}
 
-	public static class FacebookUrl extends GenericUrl {
+	public static class DailyMotionUrl extends GenericUrl {
 
-		public FacebookUrl(String encodedUrl) {
+		public DailyMotionUrl(String encodedUrl) {
 			super(encodedUrl);
 		}
 
@@ -41,24 +44,22 @@ public class App {
 						request.setParser(new JsonObjectParser(JSON_FACTORY));
 					}
 				});
-		FacebookUrl url = new FacebookUrl(
+		DailyMotionUrl url = new DailyMotionUrl(
 				"https://api.facebook.com/method/fql.query?format=json&query=select%20like_count%20from%20link_stat%20WHERE%20url%20%3D%27www.facebook.com/TEDxUBA%27");
-		// url.fields = "id,tags,title,url";
+		//url.fields = "id,tags,title,url";
 		HttpRequest request = requestFactory.buildGetRequest(url);
-		System.out.println(request.execute().parseAsString());
+		while (true) {
+			VideoFeed[] videoFeed = request.execute().parseAs(VideoFeed[].class);
+			System.out.println("Like count: " + videoFeed[0].likeCount);
+			Thread.sleep(1000);
+		}
 	}
 
 	public static void main(String[] args) {
 		try {
-			try {
-				run();
-				return;
-			} catch (HttpResponseException e) {
-				System.err.println(e.getMessage());
-			}
-		} catch (Throwable t) {
-			t.printStackTrace();
+			run();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.exit(1);
 	}
 }
